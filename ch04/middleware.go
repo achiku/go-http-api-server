958e6fd35d9f7918454f21fabf3974045f0304e3
Akira Chiku <akira.chiku@gmail.com>
2017-04-02 21:55:47 +0900

    Add ch04

---
+++ ch04/middleware.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t1 := time.Now()
+		next.ServeHTTP(w, r)
+		t2 := time.Now()
+		t := t2.Sub(t1)
+		log.Printf("[%s] %s %s", r.Method, r.URL, t.String())
+	})
+}
+
+func appLoggingMiddleware(logger *log.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			t1 := time.Now()
+			next.ServeHTTP(w, r)
+			t2 := time.Now()
+			t := t2.Sub(t1)
+			logger.Printf("[%s] %s %s", r.Method, r.URL, t.String())
+		})
+	}
+}
